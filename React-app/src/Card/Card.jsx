e50e8db9b67d5af1485b50b08d5e03ca3664938d
Yisen Cui <cuiyisen@Yisens-MacBook-Pro.local>
2026-02-03 22:21:50 +1300

    added flip animation

--- React-app/src/Card/Card.jsx
+++ React-app/src/Card/Card.jsx
@@ -1,13 +1,29 @@
 import './Card.css'
 import viteLogo from '/vite.svg'
+import React, {useState} from 'react';
 
 
 function Card(prop) {
 
+  const [flipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  }
+
   return (
-    <div className='card'>
-      <img alt={prop.name} src={prop.image}/>
+    <div className={`card ${flipped ? 'flipped' : ''}`}>
+       <div className='card-inner' onClick={handleClick}>
+        <div className='card-front'>
+          <p>&#128151;</p>
+        </div>
+        <div className='card-back'>
+          <p>temp</p>
+        </div>
+        
+      </div>
     </div>
+   
   )
 }
 
